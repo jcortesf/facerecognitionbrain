a00d4b333a2c73821acd44538782f90c52d26e76
jcortesf <juliocortesf@gmail.com>
2020-06-20 17:16:47 -0400

    Updates

---
+++ precache-manifest.22220357a33ad54abe5c7f5ac6d4e015.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d2d615d31ffedd41bef0f94bbbdcd0",
+    "url": "/facerecognitionbrain/index.html"
+  },
+  {
+    "revision": "cc7c2f1207fd6c8e6263",
+    "url": "/facerecognitionbrain/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "88193c82646d05f2b991",
+    "url": "/facerecognitionbrain/static/css/main.b53e6720.chunk.css"
+  },
+  {
+    "revision": "cc7c2f1207fd6c8e6263",
+    "url": "/facerecognitionbrain/static/js/2.c66d89e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facerecognitionbrain/static/js/2.c66d89e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88193c82646d05f2b991",
+    "url": "/facerecognitionbrain/static/js/main.14252fe4.chunk.js"
+  },
+  {
+    "revision": "cf6990c032c001c83906",
+    "url": "/facerecognitionbrain/static/js/runtime-main.fdc8abc9.js"
+  }
+]);
